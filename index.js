4e41fcbaf345301b9ea7708aa7b4740ce4bf7a08
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-11-24 19:40:45 +0100

    beers finished

--- index.js
+++ index.js
@@ -4,35 +4,20 @@ function loadBeers() {
       return response.json();
     })
     .then((data) => {
-      const name = data[0].name;
-      const img = data[0].image_url;
-      const desc = data[0].description;
+      // const name = data[0].name;
+      // const img = data[0].image_url;
+      // const desc = data[0].description;
       const cards = document.getElementsByClassName("card");
       for (let i = 0; i < 3; i++) {
         const card = cards.item(i);
         const imgEl = card.querySelector("img");
-        imgEl.setAttribute("src", img);
+        imgEl.setAttribute("src", data[i].image_url);
         const nameEl = card.querySelector("h3");
-        nameEl.innerHTML = name;
+        nameEl.innerHTML = data[i].name;
         const descEl = card.querySelector("p");
-        descEl.innerHTML = desc;
+        descEl.innerHTML = data[i].description;
       }
     })
     .catch((err) => console.log(err));
 }
 loadBeers();
-// index.js
-/*
-fetch("https://api.punkapi.com/v2/beers")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((launchObj) => {
-      const patchImage = launchObj.links.patch.small;
-      const imgElement = document.createElement("img");
-
-      imgElement.setAttribute("src", patchImage);
-      imgElement.setAttribute("width", 200);
-      document.body.appendChild(imgElement);
-    });
-  })
-  .catch((err) => console.log(err));*/
