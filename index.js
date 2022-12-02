8b9a263ff4eb49f03b845c885056299d1ef60866
Cristina Palomeque <cristina.palomeque@gmail.com>
2022-12-02 18:52:51 +0100

    current date

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ function loadBeers() {
       // const name = data[0].name;
       // const img = data[0].image_url;
       // const desc = data[0].description;
+      // tenir 3 index random entre 0 i data.length-1;
+      const idxs = [];
+      idxs.push(Math.floor(Math.random() * (data.length - 2)));
+      idxs.push(idxs[0] + 1);
+      idxs.push(idxs[0] + 2);
       const cards = document.getElementsByClassName("card");
       for (let i = 0; i < 3; i++) {
         const card = cards.item(i);
         const imgEl = card.querySelector("img");
-        imgEl.setAttribute("src", data[i].image_url);
+        imgEl.setAttribute("src", data[idxs[i]].image_url);
         const nameEl = card.querySelector("h3");
-        nameEl.innerHTML = data[i].name;
+        nameEl.innerHTML = data[idxs[i]].name;
         const descEl = card.querySelector("p");
-        descEl.innerHTML = data[i].description;
+        descEl.innerHTML = data[idxs[i]].description;
+        const anchorEl = card.querySelector("a");
+        anchorEl.setAttribute("href", `index-projects.html?project=${idxs[i]}`);
       }
     })
     .catch((err) => console.log(err));
@@ -69,5 +76,11 @@ hamburger.addEventListener("click", () => {
 
 function initProjectPage() {
   let currentDate = new Date();
-  document.getElementById("date").innerHTML = currentDate;
+  document.getElementById("date").innerHTML = currentDate.toLocaleDateString();
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams.get("project"));
+  // fetch de les beers
+  // agafar la posicio de la variable project
+  // canviar els elements del dom amb els valors de beers[project]
 }
